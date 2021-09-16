9fe988b40ce0ee4cf902af079fcd9d759db9b261
risano <muhammadrisano@gmail.com>
2021-09-16 09:11:44 +0700

    add domain cookie4

--- pages/api/login.js
+++ pages/api/login.js
@@ -12,7 +12,7 @@ const login = (req, res)=>{
       res.setHeader('Set-Cookie', cookie.serialize('token',result.token, {
         httpOnly: true,
         secure: true,
-        sameSite: 'none',
+        sameSite: 'strict',
         maxAge: 1000*60*12,
         path: '/',
         domain: '.fwebdev.xyz',
